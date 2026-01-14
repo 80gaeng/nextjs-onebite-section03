5ed1452a393ad0561e38b5b7ef9ce79970130dcc
신민경 <80gaeng@hanmail.net>
2026-01-15 08:58:13 +0900

    공부중

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,5 +1,6 @@
 import ClientComponent from './client-component';
 import styles from './page.module.css';
+import ServerComponent from './server-component';
 
 export default function Home() {
   return (
